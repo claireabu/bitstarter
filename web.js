a29744cb04ec286c42d2ad2c5d0ac6cb9333b9c5
Claire Abu-Hakima <claire.abuhakima@gmail.com>
2013-07-12 05:57:49 +0000

    Updated web.js to read from index.html

--- web.js
+++ web.js
@@ -1,9 +1,11 @@
 var express = require('express');
 
+var buffer =  new Buffer(fs.readfile('index.html'));
+var string = buf.toSring(buffer); 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(string);
 });
 
 var port = process.env.PORT || 5000;
