e31382354ff02e08d6849dad2f1be1ceeb2f6f11
Claire Abu-Hakima <claire.abuhakima@gmail.com>
2013-07-12 06:26:52 +0000

    Updated web.js to read from index.html edit5

--- web.js
+++ web.js
@@ -1,9 +1,6 @@
 var express = require('express');
 
-var buff =  new Buffer(fs.readFileSync('index.html', function(err,data){
-    if (err) throw err;
-    console.log(data);
-}));
+var buff =  new Buffer(fs.readFileSync('index.html');
 
 var text  = buff.toString();
 
